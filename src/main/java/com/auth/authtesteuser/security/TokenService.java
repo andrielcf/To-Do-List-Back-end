403f487a3e764c909110092d6bc9619ad28c5566
Anony <167723839+andrielcf@users.noreply.github.com>
2024-11-09 03:51:35 -0300

    feat(TokenService): add functionality to decode token and extract user email subject

--- src/main/java/com/auth/authtesteuser/security/TokenService.java
+++ src/main/java/com/auth/authtesteuser/security/TokenService.java
@@ -13,6 +13,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @Service
 public class TokenService {
@@ -54,5 +55,21 @@ public class TokenService {
 
     private Instant genExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String extractSubject(String token){
+
+        token = token.replace("Bearer ", "");
+        
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                .withIssuer("auth-api")
+                .build()
+                .verify(token);
+            return decodedJWT.getSubject();
+        }catch(JWTVerificationException exception){
+            throw new RuntimeException("Invalid token or expired: ", exception);
+        }
     }
 }
