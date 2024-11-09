5e643902cf6b9dcef6972efbcbb2c8fc7b93adad
Anony <167723839+andrielcf@users.noreply.github.com>
2024-11-09 03:50:03 -0300

    feat(UserRepository): update UserDetails to User for email search

--- src/main/java/com/auth/authtesteuser/repository/UserRepository.java
+++ src/main/java/com/auth/authtesteuser/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.auth.authtesteuser.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import com.auth.authtesteuser.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
  
-    UserDetails findByEmail(String email);
+    User findByEmail(String email);
     
 }
